1b964862c21294cf565c9fc7a8f6ddaaa6e86940
Nakii <guest1@Naakiizs-MacBook-Air.local>
2024-09-13 15:39:21 +0800

    add navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,68 @@
+"use client"
+import React, { useState } from "react";
+
+const MenuItems = () => (
+  <>
+    <a href="" aria-label="Home">
+      Нүүр
+    </a>
+    <a href="" aria-label="About Us">
+      Бидний тухай
+    </a>
+    <a href="" aria-label="Brand">
+      Брэнд
+    </a>
+    <a href="" aria-label="Career">
+      Карьер
+    </a>
+    <a href="" aria-label="News">
+      Мэдээ, мэдээлэл
+    </a>
+    <a href="" aria-label="Contact">
+      Холбогдох
+    </a>
+    <a href="" aria-label="Social Impact">
+      Нийгмийн үзүүлэх нөлөө
+    </a>
+    <a href="" aria-label="English">
+      EN
+    </a>
+  </>
+);
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="bg-[#1E1E1E] transition-all duration-1000">
+      <div className="flex max-w-[1400px] justify-between mx-auto items-center text-white">
+        <h1 className="text-lg font-bold">logo</h1>
+        <div className="max-md:hidden md:flex justify-between w-[85%] font-extrabold text-[16px] md:font-semibold md:w-[90%] md:text-[14px]">
+          <MenuItems />
+        </div>
+        <div className="flex md:hidden items-center">
+          <button
+            className={`menu-btn m-4 ${isMenuOpen ? "active" : ""}`}
+            onClick={toggleMenu}
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          >
+            <span className="bar bar1"></span>
+            <span className="bar bar2"></span>
+            <span className="bar bar3"></span>
+          </button>
+        </div>
+      </div>
+      <div
+        className={`flex-col text-white md:hidden flex transition-all duration-1000 ${
+          isMenuOpen ? "max-h-screen" : "max-h-0 overflow-hidden"
+        }`}
+      >
+        <MenuItems />
+      </div>
+    </nav>
+  );
+}
