ac40bffdc02791176ae66869c2900d54be5c2fe7
Nakii <naakii0603@gmail.com>
2024-10-09 02:06:26 +0800

    test

--- app/components/LaazLogo.jsx
+++ app/components/LaazLogo.jsx
@@ -3,10 +3,10 @@ const GIF = [{ gif: "/GIF/Pepsi.gif" }];
 
 export default function LaazLogo() {
   return (
-    <div className="pepsi ">
+    <div className="pepsi relative">
       {GIF.map((item, index) => (
         <img
-          className="  w-[300px] h-[300px] mt-[64px]"
+          className=" absolute w-[300px] h-[300px] mt-[64px]"
           key={index}
           src={item.gif}
           alt="Pepsi GIF"
